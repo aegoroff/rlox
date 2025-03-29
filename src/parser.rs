541671b4efdd1092fb0cc0985f3d90c290f9263e
Alexander Egorov <egoroff@gmail.com>
2025-03-29 09:19:50 +0300

    feat: parser draft

---
+++ src/parser.rs
@@ -0,0 +1,28 @@
+use crate::lexer::Token;
+
+pub trait Visitor {
+    fn visit_literal(&self, literal: &Literal);
+    fn visit_binary_expr(&self, binary: &BinaryExpr);
+}
+
+pub trait Expr {
+    fn visit<V: Visitor>(&self, visitor: V);
+}
+
+pub struct Literal<'a> {
+    pub token: Token<'a>,
+}
+
+impl Expr for Literal<'_> {
+    fn visit<V: Visitor>(&self, visitor: V) {
+        visitor.visit_literal(self);
+    }
+}
+
+pub struct BinaryExpr {}
+
+impl Expr for BinaryExpr {
+    fn visit<V: Visitor>(&self, visitor: V) {
+        visitor.visit_binary_expr(self);
+    }
+}
