ca4853ffa8e97dcca55d3598d89d2a59f06b4ad4
Alexander Egorov <egoroff@gmail.com>
2025-03-29 12:11:53 +0300

    feat: Unary expression draft added to parser

--- src/parser.rs
+++ src/parser.rs
@@ -3,6 +3,7 @@ use crate::lexer::Token;
 pub trait Visitor {
     fn visit_literal(&self, literal: &Literal);
     fn visit_binary_expr(&self, binary: &BinaryExpr);
+    fn visit_unary_expr(&self, binary: &UnaryExpr);
 }
 
 pub trait Expr {
@@ -24,5 +25,13 @@ pub struct BinaryExpr {}
 impl Expr for BinaryExpr {
     fn visit<V: Visitor>(&self, visitor: V) {
         visitor.visit_binary_expr(self);
+    }
+}
+
+pub struct UnaryExpr {}
+
+impl Expr for UnaryExpr {
+    fn visit<V: Visitor>(&self, visitor: V) {
+        visitor.visit_unary_expr(self);
     }
 }
