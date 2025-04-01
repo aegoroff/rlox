212326367c130896957b1331fbc2bb62e898f17f
Alexander Egorov <egoroff@gmail.com>
2025-04-01 11:25:43 +0300

    feat: Statements visitor corrected

--- src/parser.rs
+++ src/parser.rs
@@ -39,12 +39,12 @@ impl<'a> Expr<'a> {
     pub fn accept<R>(&self, visitor: impl ExprVisitor<'a, R>) -> R {
         match self {
             Expr::Literal(token) => visitor.visit_literal(token),
-            Expr::Binary(token, left, right) => visitor.visit_binary_expr(token, left, right),
-            Expr::Unary(token, expr) => visitor.visit_unary_expr(token, expr),
-            Expr::Assign(token, expr) => visitor.visit_assign_expr(token, expr),
-            Expr::Call(token, expr, exprs) => visitor.visit_call_expr(token, expr, exprs),
+            Expr::Binary(operator, left, right) => visitor.visit_binary_expr(operator, left, right),
+            Expr::Unary(operator, expr) => visitor.visit_unary_expr(operator, expr),
+            Expr::Assign(name, value) => visitor.visit_assign_expr(name, value),
+            Expr::Call(paren, callee, args) => visitor.visit_call_expr(paren, callee, args),
             Expr::Get(name, object) => visitor.visit_get_expr(name, object),
-            Expr::Grouping(expr) => visitor.visit_grouping_expr(expr),
+            Expr::Grouping(grouping) => visitor.visit_grouping_expr(grouping),
             Expr::Logical(token, left, right) => visitor.visit_logical_expr(token, left, right),
             Expr::Set(name, obj, val) => visitor.visit_set_expr(name, obj, val),
             Expr::Super(keyword, method) => visitor.visit_super_expr(keyword, method),
@@ -71,16 +71,44 @@ pub enum Stmt<'a> {
     While(Expr<'a>, Box<Stmt<'a>>),
 }
 
+impl<'a> Stmt<'a> {
+    pub fn accept<R>(&self, visitor: impl StmtVisitor<'a, R>) -> R {
+        match self {
+            Stmt::Block(body) => visitor.visit_block_stmt(body),
+            Stmt::Class(name, superclass, methods) => {
+                visitor.visit_class_stmt(name, superclass, methods)
+            }
+            Stmt::Expression(expr) => visitor.visit_expression_stmt(expr),
+            Stmt::Function(token, params, body) => visitor.visit_function_stmt(token, params, body),
+            Stmt::If(cond, then, otherwise) => visitor.visit_if_stmt(cond, then, otherwise),
+            Stmt::Print(expr) => visitor.visit_print_stmt(expr),
+            Stmt::Return(keyword, value) => visitor.visit_return_stmt(keyword, value),
+            Stmt::Variable(name, initializer) => visitor.visit_variable_stmt(name, initializer),
+            Stmt::While(cond, body) => visitor.visit_while_stmt(cond, body),
+        }
+    }
+}
+
 pub trait StmtVisitor<'a, R> {
-    fn visit_block_stmt(&self, block: &Stmt<'a>) -> R;
-    fn visit_class_stmt(&self, class: &Stmt<'a>) -> R;
-    fn visit_expression_stmt(&self, expr: &Stmt<'a>) -> R;
-    fn visit_function_stmt(&self, function: &Stmt<'a>) -> R;
-    fn visit_if_stmt(&self, if_stmt: &Stmt<'a>) -> R;
-    fn visit_print_stmt(&self, print: &Stmt<'a>) -> R;
-    fn visit_return_stmt(&self, ret: &Stmt<'a>) -> R;
-    fn visit_variable_stmt(&self, variable: &Stmt<'a>) -> R;
-    fn visit_while_stmt(&self, while_stmt: &Stmt<'a>) -> R;
+    fn visit_block_stmt(&self, body: &[Box<Stmt<'a>>]) -> R;
+    fn visit_class_stmt(
+        &self,
+        name: &Token<'a>,
+        superclass: &Stmt<'a>,
+        methods: &[Box<Stmt<'a>>],
+    ) -> R;
+    fn visit_expression_stmt(&self, expr: &Expr<'a>) -> R;
+    fn visit_function_stmt(
+        &self,
+        token: &Token<'a>,
+        params: &[Box<Stmt<'a>>],
+        body: &[Box<Stmt<'a>>],
+    ) -> R;
+    fn visit_if_stmt(&self, cond: &Expr<'a>, then: &Stmt<'a>, otherwise: &Stmt<'a>) -> R;
+    fn visit_print_stmt(&self, expr: &Expr<'a>) -> R;
+    fn visit_return_stmt(&self, keyword: &Token<'a>, value: &Expr<'a>) -> R;
+    fn visit_variable_stmt(&self, name: &Token<'a>, initializer: &Expr<'a>) -> R;
+    fn visit_while_stmt(&self, cond: &Expr<'a>, body: &Stmt<'a>) -> R;
 }
 
 pub struct AstPrinter {}
