1d30562daeb35c2ce41dca570a42cae1d0a8cc49
Alexander Egorov <egoroff@gmail.com>
2025-04-14 22:05:17 +0300

    feat: environment support nesting now

--- src/env.rs
+++ src/env.rs
@@ -5,18 +5,29 @@ use std::collections::HashMap;
 #[derive(Default)]
 pub struct Environment<'a> {
     storage: HashMap<&'a str, LoxValue>,
+    enclosing: Option<Box<Environment<'a>>>,
 }
 
 impl<'a> Environment<'a> {
     pub fn new() -> Self {
         Self {
             storage: HashMap::new(),
+            enclosing: None,
+        }
+    }
+
+    pub fn child(enclosing: Box<Environment<'a>>) -> Self {
+        Self {
+            storage: HashMap::new(),
+            enclosing: Some(enclosing),
         }
     }
 
     pub fn get(&'a self, id: &'a str) -> miette::Result<&'a LoxValue> {
         if let Some(var) = self.storage.get(id) {
             Ok(var)
+        } else if let Some(enclosing) = &self.enclosing {
+            enclosing.get(id)
         } else {
             Err(miette!("Undefined variable: '{id}'"))
         }
