da9a13db28fd06767d1bb28f18de44e822678589
Alexander Egorov <egoroff@gmail.com>
2025-04-25 00:52:12 +0300

    refactor: renamings to improve code readability

--- src/env.rs
+++ src/env.rs
@@ -4,27 +4,27 @@ use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 #[derive(Default)]
 pub struct Environment {
-    storage: HashMap<String, LoxValue>,
+    values: HashMap<String, LoxValue>,
     enclosing: Option<Rc<RefCell<Environment>>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Self {
-            storage: HashMap::new(),
+            values: HashMap::new(),
             enclosing: None,
         }
     }
 
     pub fn child(enclosing: Rc<RefCell<Environment>>) -> Self {
         Self {
-            storage: HashMap::new(),
+            values: HashMap::new(),
             enclosing: Some(enclosing),
         }
     }
 
     pub fn get(&self, id: &str) -> miette::Result<LoxValue> {
-        if let Some(var) = self.storage.get(id) {
+        if let Some(var) = self.values.get(id) {
             Ok(var.clone())
         } else if let Some(enclosing) = &self.enclosing {
             enclosing.borrow().get(id)
@@ -37,13 +37,13 @@ impl Environment {
         if distance == 0 {
             self.get(id)
         } else {
-            let mut child = self.enclosing.clone();
+            let mut parent = self.enclosing.clone();
             for _ in 1..distance {
-                if let Some(e) = child {
-                    child = e.borrow().enclosing.clone();
+                if let Some(e) = parent {
+                    parent = e.borrow().enclosing.clone();
                 }
             }
-            if let Some(e) = child {
+            if let Some(e) = parent {
                 e.borrow().get(id)
             } else {
                 Err(miette!("Undefined identifier: '{id}'"))
@@ -52,16 +52,16 @@ impl Environment {
     }
 
     pub fn define(&mut self, id: String, initializer: LoxValue) {
-        if self.storage.contains_key(&id) {
+        if self.values.contains_key(&id) {
             let _ = self.assign(id, initializer);
         } else {
-            self.storage.entry(id).or_insert(initializer);
+            self.values.entry(id).or_insert(initializer);
         }
     }
 
     pub fn assign(&mut self, id: String, initializer: LoxValue) -> miette::Result<()> {
-        if self.storage.contains_key(&id) {
-            self.storage.entry(id).and_modify(|e| *e = initializer);
+        if self.values.contains_key(&id) {
+            self.values.entry(id).and_modify(|e| *e = initializer);
             Ok(())
         } else if let Some(enclosing) = &mut self.enclosing {
             enclosing.borrow_mut().assign(id, initializer)
@@ -79,13 +79,13 @@ impl Environment {
         if distance == 0 {
             self.assign(id, initializer)
         } else {
-            let mut child = self.enclosing.clone();
+            let mut parent = self.enclosing.clone();
             for _ in 1..distance {
-                if let Some(e) = child {
-                    child = e.borrow().enclosing.clone();
+                if let Some(e) = parent {
+                    parent = e.borrow().enclosing.clone();
                 }
             }
-            if let Some(e) = child {
+            if let Some(e) = parent {
                 e.borrow_mut().assign(id, initializer)
             } else {
                 Err(miette!("Undefined identifier: '{id}'"))
