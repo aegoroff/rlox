9876d5d1b07f4a4fdcb8a87e3ba5c4b43f92ffbd
Alexander Egorov <egoroff@gmail.com>
2025-05-05 14:27:26 +0300

    feat: constants opcode draft

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -1,13 +1,23 @@
 use std::fmt::Display;
 
 pub enum OpCode {
-    Return = 0,
+    Return,
+    Constant(usize),
+}
+
+#[derive(Clone, Debug)]
+pub enum LoxValue {
+    String(String),
+    Number(f64),
+    Bool(bool),
+    Nil,
 }
 
 impl Display for OpCode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             OpCode::Return => write!(f, "OP_RETURN"),
+            OpCode::Constant(index) => write!(f, "OP_CONSTANT {}", index),
         }
     }
 }
@@ -15,17 +25,24 @@ impl Display for OpCode {
 #[derive(Default)]
 pub struct Chunk {
     instructions: Vec<OpCode>,
+    constants: Vec<LoxValue>,
 }
 
 impl Chunk {
     pub fn new() -> Self {
         Self {
             instructions: vec![],
+            constants: vec![],
         }
     }
 
     pub fn write_code(&mut self, code: OpCode) {
         self.instructions.push(code);
+    }
+
+    pub fn add_constant(&mut self, value: LoxValue) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
     }
 
     pub fn disasembly(&self, name: &str) {
