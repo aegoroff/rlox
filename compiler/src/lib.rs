3b0e0d186cd5ad71f384e4ab4a1bf89aee7e015e
Alexander Egorov <egoroff@gmail.com>
2025-05-19 00:32:51 +0300

    perf: unused error code removed

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -10,7 +10,6 @@ extern crate num_derive;
 pub type Result<T, E = miette::Report> = core::result::Result<T, E>;
 
 pub enum ProgramError {
-    Compile(String),
     Runtime(String),
     InvalidInstruction(usize),
     InstructionsStackEmpty,
@@ -26,7 +25,6 @@ pub enum ProgramError {
 impl Display for ProgramError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ProgramError::Compile(s) => write!(f, "{s}"),
             ProgramError::Runtime(s) => write!(f, "{s}"),
             ProgramError::InvalidInstruction(code) => write!(f, "Invalid instruction: {code}"),
             ProgramError::InstructionsStackEmpty => write!(f, "Instructions stack empty"),
