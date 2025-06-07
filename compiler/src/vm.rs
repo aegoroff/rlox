efef3d51b64bd6b4dc850ed1e1e276a9e99fac14
Alexander Egorov <egoroff@gmail.com>
2025-06-07 15:18:50 +0300

    test: one more closure test added

--- compiler/src/vm.rs
+++ compiler/src/vm.rs
@@ -904,6 +904,30 @@ var closure = Foo().getClosure();
 print closure()()(); // expect: Foo
 "#, "Foo" ; "this nested closure")]
     #[test_case(r#"
+var f;
+
+fun f1() {
+  var a = "a";
+  fun f2() {
+    var b = "b";
+    fun f3() {
+      var c = "c";
+      fun f4() {
+        print a;
+        print b;
+        print c;
+      }
+      f = f4;
+    }
+    f3();
+  }
+  f2();
+}
+f1();
+
+f();
+"#, "a\nb\nc" ; "closure nested closure")]
+    #[test_case(r#"
 class Foo {
   getClosure() {
     fun closure() {
