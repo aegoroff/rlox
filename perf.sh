ffb19cf567b36eefbf5fd5a5c4e5920dca9aed24
Alexander Egorov <egoroff@gmail.com>
2025-12-03 08:03:59 +0300

    chore: line added to perf script to separate different benches

--- perf.sh
+++ perf.sh
@@ -3,10 +3,11 @@ BIN_PATH="/home/egr/code/rlox/target/release/rlox"
 REF_PATH="/home/egr/code/craftinginterpreters/build/clox"
 
 function perf() {
-	target=$1
-	tool=$2
-	ref_key=$3
-	$tool "$BIN_PATH c f $target" $ref_key "$REF_PATH $target"
+  target=$1
+  tool=$2
+  ref_key=$3
+  $tool "$BIN_PATH c f $target" $ref_key "$REF_PATH $target"
+  echo "--------------------------------------------------------------------------------------------"
 }
 
 perf $TARGET poop
